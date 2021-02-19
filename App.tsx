2c44ca5471a93b14cfc00fc5c51ba36b33deaa67
cruzach <charliecruzan@gmail.com>
2021-02-19 13:29:57 -0500

    sentry

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import * as Sentry from "sentry-expo";
+Sentry.init({
+  dsn:
+    "https://762a0e6b564344e6912e41d44ea56ac5@o237685.ingest.sentry.io/1549119",
+  enableInExpoDevelopment: true,
+  debug: true,
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="throw again"
+        onPress={() => {
+          throw new Error("uh oh again");
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
