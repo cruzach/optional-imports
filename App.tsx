920c04499f739771b438359a79370e48fe7ea1d8
cruzach <charliecruzan@gmail.com>
2021-03-29 15:41:28 -0400

    tmp

--- App.tsx
+++ App.tsx
@@ -1,16 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import * as Sentry from "sentry-expo";
-
-Sentry.init({
-  dsn:
-    "https://762a0e6b564344e6912e41d44ea56ac5@o237685.ingest.sentry.io/1549119",
-  enableInExpoDevelopment: true,
-  debug: true,
-  enableAutoSessionTracking: true,
-});
-
+import * as something from "stripe-react-native";
+console.log(something);
 export default function App() {
   return (
     <View style={styles.container}>
@@ -20,10 +12,6 @@ export default function App() {
         onPress={() => {
           throw new Error("yippeee");
         }}
-      />
-      <Button
-        title={"hi"}
-        onPress={() => Sentry.Browser.captureException("helloooo")}
       />
     </View>
   );
